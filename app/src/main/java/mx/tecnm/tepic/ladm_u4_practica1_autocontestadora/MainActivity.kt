a0d1386a60923c43a515ace59a111c8aee570059
Jorge varela <jojovarelaga@ittepic.edu.mx>
2021-11-29 21:29:10 -0600

    auto cotestadora

--- app/src/main/java/mx/tecnm/tepic/ladm_u4_practica1_autocontestadora/MainActivity.kt
+++ app/src/main/java/mx/tecnm/tepic/ladm_u4_practica1_autocontestadora/MainActivity.kt
@@ -3,12 +3,22 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CallLog
+import android.telephony.SmsManager
+import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     val siPermiso = 1
     var mensajeEnvio =""
+    val Permiso_Telefono = 2
+    var hilo = Hilo(this)
+    var Datos = ArrayList<Persona>()
+    var ID_Llamada = ""
+    var Inicio = true
+    var BaseRemota = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,7 +45,14 @@ class MainActivity : AppCompatActivity() {
         button3.setOnClickListener {
             startActivity(Intent(this, MainActivity4::class.java))
         }
-
+        button4.setOnClickListener {
+            /*PERMISO - LEER LLAMADAS*/
+            if(ActivityCompat.checkSelfPermission(this,
+                    android.Manifest.permission.READ_CALL_LOG)!= PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_CALL_LOG), Permiso_Telefono)
+            }
+            hilo.start()
+        }
     }
 
     override fun onRequestPermissionsResult(
@@ -49,5 +66,80 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    public fun Accion(){
 
+        var Cursor = contentResolver.query(CallLog.Calls.CONTENT_URI,null,null,null,"date DESC")
+
+        if(Cursor!!.moveToFirst()){
+            var Num_Telefono = Cursor.getColumnIndex(CallLog.Calls.NUMBER)
+            var Tipo_Llamada = Cursor.getColumnIndex(CallLog.Calls.TYPE)
+            var ID = Cursor.getColumnIndex(CallLog.Calls._ID)
+
+            if(Inicio){ID_Llamada = Cursor.getString(ID)
+                Inicio = false}
+
+            if (Cursor.getString(ID) != ID_Llamada){
+                // El Tipo "3" corresponde a las llamadas perdidas o no contestadas
+                if (Cursor.getInt(Tipo_Llamada) == 3){
+                    for (i in 0..(Datos.size-1)){
+                        //Si el numero de telefono coincide con alguno de nuestras listas
+                        if(Cursor.getString(Num_Telefono) == Datos.get(i).Telefono){
+                            Enviar_SMS(Datos.get(i))
+                            break
+                        }//if
+                    }//for
+                }//if
+                ID_Llamada = Cursor.getString(ID)
+            }
+        }
+    }//Accion
+
+    private fun Enviar_SMS(persona: Persona){
+        var Mensaje = "NO DEVOLVERE TU LLAMADA, POR FAVOR NO INSISTAS"
+
+        if (persona.Deseado.equals("LISTA BLANCA")){
+            Mensaje = "NO ESTOY DISPONIBLE AL RATO TE HABLO"
+        } else {
+            var datosInsertar = hashMapOf(
+                "NUMERO DESEADO" to "NO",
+                "Nombre Contacto" to persona.Nombre,
+                "Telefono" to persona.Telefono
+            )
+
+            BaseRemota.collection("Contestadora")
+                .add(datosInsertar)
+                .addOnSuccessListener {}
+                .addOnFailureListener {
+                    Mensaje("ERROR NO DESEADOS: ${it.message!!}")
+                }
+        }
+
+        SmsManager.getDefault().sendTextMessage(persona.Telefono,null,
+            Mensaje,null,null)
+    }//Enviar_SMS
+
+    private fun Mensaje(s: String) {
+        AlertDialog.Builder(this)
+            .setTitle("ATENCIÓN")
+            .setMessage(s)
+            .setPositiveButton("OK"){d,i->
+
+            }
+            .show()
+    }//Mensaje
 }
+
+class Hilo(p:MainActivity) : Thread(){
+
+    val Puntero = p
+
+    override fun run() {
+        super.run()
+        while (true){
+            sleep(1000)
+            Puntero.runOnUiThread {
+                Puntero.Accion()
+            }
+        }
+    }
+}//class Hilo
---
+++ app/src/main/java/mx/tecnm/tepic/ladm_u4_practica1_autocontestadora/Persona.kt
@@ -0,0 +1,7 @@
+package mx.tecnm.tepic.ladm_u4_practica1_autocontestadora
+
+class Persona {
+    var Deseado = ""
+    var Nombre = ""
+    var Telefono = ""
+}
